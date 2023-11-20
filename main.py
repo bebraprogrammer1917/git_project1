504eb0568eee3c8bed7e7f23a720e5509b0c763b
Arthur <karakkhiretdinov@gmil.com>
2023-11-20 16:39:56 +0500

    третий коммит

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     print('My first git program')
     print('Hello, world!')
     print('pipka')
-
+    print('Again')
+    print('UFO came and added this line')
 
 if __name__ == '__main__':
     main()
